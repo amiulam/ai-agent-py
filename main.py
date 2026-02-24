8aed858341593c5a1029c35a69614fd058a2c7b5
amiulam <amiulam12@gmail.com>
2026-02-24 11:21:05 +0700

    feat: google genai

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
 def main():
     print("Hello from ai-agent-py!")
+    if api_key is None:
+        raise RuntimeError("no api key provided")
+
+    object = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    print(object.text)
 
 
 if __name__ == "__main__":
